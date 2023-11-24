f1cec153f919948016104bbbd4d48c2799019427
DWshka <mikhailsmailbox@gmail.com>
2023-11-24 22:20:32 +0300

    Добавлен файл Laba_3.py

---
+++ Laba_3.py
@@ -0,0 +1,31 @@
+import re
+def check_snils(snils):
+    pattern = re.compile(r'^\d{3}-\d{3}-\d{3} \d{2}$') #Шаблон снился
+    last_Two = snils[-2:] # 2 Последних числа
+
+
+    if pattern.match(snils):
+
+        l = snils.replace("-", "")
+        control_sum = 0 #Контрольная сумма
+
+        for i in range(0, 9):
+            control_sum = int(l[i]) * (9 - i) + control_sum
+
+        if control_sum <= 101 and control_sum == int(last_Two):
+            return f"{snils} | Является СНИЛСом"
+
+        if control_sum > 101 and control_sum % 101 == int(last_Two):
+            return f"{snils} | Является СНИЛСом"
+
+    return f"{snils} | Не является СНИЛСом"
+
+
+while(True):
+    answer_user = input() 
+    if answer_user == "stop":
+        exit(0)
+
+    print(check_snils(answer_user))
+
+    
